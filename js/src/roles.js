f3e8ad5eb9fc857dbec7afaae8e06941951c0a22
ryanbmarx <ryanbmarx@gmail.com>
2018-05-10 13:57:59 -0500

    add a special sendHeight execution

--- js/src/roles.js
+++ js/src/roles.js
@@ -1,8 +1,12 @@
 const pym = require('pym.js');
 // Listen for the loaded event then run the pym stuff.
-window.addEventListener('load', function(e){
-	console.log("pym, yo")
+window.addEventListener('DOMContentLoaded', function(e){
+	console.log("pym, yo, stap")
 	window.pymChild = new pym.Child({});
 	// pymChild.sendMessage('childLoaded');
+	window.pymChild.sendHeight();
+});
+
+window.addEventListener('load', function(e){
 	window.pymChild.sendHeight();
 });
